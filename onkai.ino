4d34b0d6526753dba2e3a63b691a7328ad99c0a4
IMAGELAB\isikoro <16kmc04@ms.dendai.ac.jp>
2017-04-14 16:47:33 +0900

    微調整

--- onkai.ino
+++ onkai.ino
@@ -1,7 +1,9 @@
 #define PINNO 11 //圧電ブザー
 
-int zennon[15] = {262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047}; //全音2オクターブ分
-int hannon[11] = {277, 311, 370, 415, 466, 554, 622, 740, 831, 932, 1109}; //半音2オクターブ分
+//int zennon[15] = {262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047}; //全音2オクターブ分
+//int hannon[11] = {277, 311, 370, 415, 466, 554, 622, 740, 831, 932, 1109}; //半音2オクターブ分
+int zennon[8] = {262, 294, 330, 350, 392, 440, 494, 524}; //全音2オクターブ分
+int hannon[6] = {278, 312, 370, 416, 466, 554}; 
 int countZEN = 0;
 int countHAN = 0;
 int toneDelay = 100;
@@ -10,11 +12,13 @@ int toneDelay = 100;
 void setup() {
   Serial.begin(9600);
   Serial.println(F("Hello!"));
-  for (int i = 0; i < 5; i++) {
-    tone(PINNO, zennon[countZEN + 6]) ;
-    delay(toneDelay + 10);
+    tone(PINNO, (zennon[countZEN + 4])*2) ;
+    delay(400);
+    tone(PINNO, (zennon[countZEN + 3])*2) ;
+    delay(200);
+    tone(PINNO, (zennon[countZEN + 7])*2) ;
+    delay(1000);
     noTone(PINNO);
-  }
 }
 //-----メイン-----
 
@@ -76,21 +80,33 @@ void loop() {
     tone(PINNO, hannon[countHAN]) ;
     delay(toneDelay);
     noTone(PINNO);
-  } else if (digitalRead(10) == HIGH) {//音階を左にシフト
+  } else if (digitalRead(10) == HIGH) {//音階を1オクターブ左にシフト
     Serial.println(F("10pin"));
-    countZEN--;
-    if (countZEN < 0 )
-      countZEN = 0;
-    else if (7 < countZEN)
-      countZEN = 7;
+//    countZEN--;
+//    if (countZEN < 0 )
+//      countZEN = 0;
+//    else if (7 < countZEN)
+//      countZEN = 7;
+    for(int i = 0;i < 8;i++){
+         zennon[i] /= 2;
+         if(i < 6)
+           hannon[i] /= 2;
+    }
+
     delay(500);
-  } else if (digitalRead(12) == HIGH) {//音階を右にシフト
+  } else if (digitalRead(12) == HIGH) {//音階を1オクターブ右にシフト
     Serial.println(F("12pin"));
-    countZEN++;
-    if (countZEN < 0 )
-      countZEN = 0;
-    else if (7 < countZEN)
-      countZEN = 7;
+//    countZEN++;
+//    if (countZEN < 0 )
+//      countZEN = 0;
+//    else if (7 < countZEN)
+//      countZEN = 7;
+//    delay(500);
+    for(int i = 0;i < 8;i++){
+         zennon[i] *= 2;
+         if(i < 6)
+           hannon[i] *= 2;
+    }
     delay(500);
   } else if (digitalRead(13) == HIGH) {//音階1オクターブ切り替え
     Serial.println(F("13pin"));
@@ -103,14 +119,14 @@ void loop() {
     }
     delay(500);
   }
-  digitalRead(2) == HIGH ? Serial.print(F("D2")) : Serial.print(F("- "));
-  digitalRead(3) == HIGH ? Serial.print(F("D3")) : Serial.print(F("- "));
-  digitalRead(4) == HIGH ? Serial.print(F("D4")) : Serial.print(F("- "));
-  digitalRead(5) == HIGH ? Serial.print(F("D5")) : Serial.print(F("- "));
-  digitalRead(6) == HIGH ? Serial.print(F("D6")) : Serial.print(F("- "));
-  digitalRead(7) == HIGH ? Serial.print(F("D7")) : Serial.print(F("- "));
-  digitalRead(8) == HIGH ? Serial.print(F("D8")) : Serial.print(F("- "));
-  digitalRead(9) == HIGH ? Serial.print(F("D9")) : Serial.print(F("- "));
+  digitalRead(2) == HIGH ? Serial.print(F("D2   ")) : Serial.print(F("-    "));
+  digitalRead(3) == HIGH ? Serial.print(F("D3   ")) : Serial.print(F("-    "));
+  digitalRead(4) == HIGH ? Serial.print(F("D4   ")) : Serial.print(F("-    "));
+  digitalRead(5) == HIGH ? Serial.print(F("D5   ")) : Serial.print(F("-    "));
+  digitalRead(6) == HIGH ? Serial.print(F("D6   ")) : Serial.print(F("-    "));
+  digitalRead(7) == HIGH ? Serial.print(F("D7   ")) : Serial.print(F("-    "));
+  digitalRead(8) == HIGH ? Serial.print(F("D8   ")) : Serial.print(F("-    "));
+  digitalRead(9) == HIGH ? Serial.print(F("D9   ")) : Serial.print(F("-    "));
   Serial.print(F("\t"));
   Serial.print(F("A5:"));
   Serial.print(analogRead(A5));
@@ -130,5 +146,17 @@ void loop() {
   Serial.print(F("A0:"));
   Serial.println(analogRead(A0));
 
+//  for(int i = 0; i < 8; i++){
+//    Serial.print(zennon[i]);
+//    Serial.print(", ");
+//  }
+//  Serial.print(F("\t"));
+//  for(int i = 0; i < 6; i++){
+//    Serial.print("   ");
+//    Serial.print(hannon[i]);
+//    Serial.print(",");
+//    Serial.print(F("\t"));
+//  }
+//  Serial.print("\n");
 
 }
